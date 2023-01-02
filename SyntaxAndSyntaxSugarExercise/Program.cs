ac89744484e4ec5f2898e6f0454a3c6aa014324d
Devin Riley <devin.riley144@gmail.com>
2023-01-02 10:46:17 -0600

    cleaned up

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,9 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+            var answer = 4;
+            Console.WriteLine((answer < 9) ? $" {answer} is less than 9" : $" {answer} is greater than or equal to nine");
         }
     }
 }
